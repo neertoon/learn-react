2e09f50019cbcf5e41747fa49d6ace51e57048d3
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2023-01-30 22:07:52 +0100

    Więcej JSXa

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-    return <h2>Expense item!</h2>
+  return (
+    <div>
+      <div>2023.01.30</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
