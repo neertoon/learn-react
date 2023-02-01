472f8c3dc1963a748f4db281c58acf139ae36846
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2023-02-01 21:27:30 +0100

    Jak dodawać CSS do reacta

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
   return (
-    <div>
+    <div className="expense-item">
       <div>2023.01.30</div>
-      <div>
+      <div class="expense-item__description">
         <h2>Car Insurance</h2>
-        <div>$294.67</div>
+        <div className="expense-item__price">$294.67</div>
       </div>
     </div>
   );
