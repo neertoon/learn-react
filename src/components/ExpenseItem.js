ea32e3777a6ac059fae14b2cabfbc3159cd59f6f
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2023-02-02 21:36:16 +0100

    Lepsze formatowanie dat

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,12 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+  let month = props.date.toLocaleString('en-US', { month: 'long' });
+  let day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  let year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>{day} {month} {year}</div>
       <div class="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
