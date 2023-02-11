458afac6ca20d949cdc897e0094c25ce79652107
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2023-02-11 18:21:39 +0100

    Przeniesienie listy wydatków do osobnego komponentu

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import ExpenseItem from "./components/ExpenseItem";
+
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
@@ -7,15 +8,12 @@ function App() {
       { id: 'e3', title: 'Pasta do butów', amount: 1234.67, date: new Date(2023, 1, 1)},
       { id: 'e4', title: 'Maść na ból d', amount: 1500.10, date: new Date(2023, 1, 2)}
   ];
-  
+
   return (
     <div>
       <h2>Let's get started!</h2>
       <span>To tez widoczne</span>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
+      <Expenses items={expenses}></Expenses>
     </div>
   );
 }
