46abb0faef1629d41a7effec7d2c7106af053bf9
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2023-02-12 21:13:24 +0100

    Pierwsza funkcja onclick

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,16 +2,23 @@ import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
+const clickEvent = function() {
+  console.log('sdfsdfsdfsdf');
+}
+
 function ExpenseItem(props) {
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
-      <div class="expense-item__description">
+      <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickEvent}>Change title</button>
     </Card>
   );
 }
+
+
 
 export default ExpenseItem;
